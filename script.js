5d28e3d490b349fbfebe75db212c96facc49f51a
David <davidifme@icloud.com>
2024-12-31 18:28:59 +0100

    add show books button functionality

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-const myLibrary = ["To Kill a Mockingbird", "1984", "The Great Gatsby"];
+const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages) {
+    let book = new Book(title, author, pages);
+    myLibrary.push(book);
 }
 
-function showLibrary() {
-    
+function showBooks() {
+    for (const book of myLibrary) {
+        const bookContainer = document.createElement("div");
+        bookContainer.classList.add('book');
+
+        const bookTitle = document.createElement('h2');
+        bookTitle.textContent = book.title;
+        bookContainer.appendChild(bookTitle);
+
+        const unorderedList = document.createElement('ul');
+        
+        const authorItem = document.createElement('li');
+        authorItem.textContent = `Author: ${book.author}`;
+        unorderedList.appendChild(authorItem);
+
+        const pagesItem = document.createElement('li');
+        pagesItem.textContent = `Pages: ${book.pages}`;
+        unorderedList.appendChild(pagesItem);
+
+        bookContainer.appendChild(unorderedList);
+        bookList.appendChild(bookContainer);
+    }
 }
+
+const bookList = document.querySelector('.book-list');
+const showBooksButton = document.querySelector('.show-books');
+
+showBooksButton.addEventListener('click', showBooks);
+
+addBookToLibrary('Title 1', 'Author 1', 261);
+addBookToLibrary('Title 2', 'Author 2', 262);
+addBookToLibrary('Title 3', 'Author 3', 263);
